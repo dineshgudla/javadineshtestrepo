e84a832525fee8c307660b7489e5504c5c64ed8c
dineshgudla <154829752+dineshgudla@users.noreply.github.com>
2025-06-11 22:12:22 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,8 @@ public class HelloWorld {
   public static void main(String[] args) {
      System.out.println("Hello, KrishnaDevOpsss");
      System.out.println("Hello, dineshaswes course");
-     System.out.println("Hello, Jenkinnsteamms"); 
+     System.out.println("Hello, Jenkinnsteamms");
+     System.out.println("Hello, dineshteams"); 
 
   }
 }
