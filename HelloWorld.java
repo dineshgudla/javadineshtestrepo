5320eccd975bce45fd6ff058d8a57a242cad92be
dineshgudla <154829752+dineshgudla@users.noreply.github.com>
2025-06-11 22:09:25 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+  public static void main(String[] args) {
+     System.out.println("Hello, KrishnaDevOpsss");
+     System.out.println("Hello, dineshaswes course");
+     System.out.println("Hello, Jenkinnsteamms"); 
+
+  }
+}
